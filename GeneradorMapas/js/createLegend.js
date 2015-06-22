b4d768f581e5bfe13bb7f5b4fea7dcca22883dae
Alex <hueskyecla@hotmail.com>
2015-06-22 16:53:59 +0200

    legend

--- GeneradorMapas/js/createLegend.js
+++ GeneradorMapas/js/createLegend.js
@@ -1,7 +1,8 @@
 // JavaScript Document
-function create_legend(layer)
+function create_legend(layer,wmsurl)
 {
+	
 	var name = layer.get('name');
-	 var codigohtml ="<img class=\"legendIcon\" src='http://localhost:8080/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER="+name+"' />";
+	 var codigohtml ="<img class=\"legendIcon\" src='"+wmsurl+"?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER="+name+"' />";
 	return codigohtml
 }
