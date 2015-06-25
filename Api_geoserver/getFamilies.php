c081c094255b9edc6f8ecb21255383e217a88ebc
Alikornio <Alikornio@hotmail.com>
2015-06-25 11:05:26 +0200

    Segun

--- Api_geoserver/getFamilies.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "ApiRest.php";
-	include "Connection.php";
-	include "LocalgisMap.php";
-
-	$idMap=537;
-	$map = new LocalgisMap($idMap);
-	print(json_encode($map));
-?>
--- Api_geoserver/getStyles.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "ApiRest.php";
-	include "Connection.php";
-	include "LocalgisLayer.php";
-
-	$idLayer=12547;
-	$layer = new LocalgisLayer($idLayer);
-	print(json_encode($layer));
-?>
