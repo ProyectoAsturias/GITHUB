91626a35ae5f583f47e5a947161f89afca75c065
Alikornio <Alikornio@hotmail.com>
2015-07-27 16:45:41 +0200

    no message

--- MapGenerator/js/legend.js
+++
@@ -1,7 +0,0 @@
-function create_legend(layer){
-	var response=layer.getSource().p;
-	console.log(response);
-	var urlWms= response.split("#");
-	var legend ="<img class=\"legendIcon\" src='"+urlWms[0]+"?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER="+layer.name+"' />";
-	return legend;
-}
