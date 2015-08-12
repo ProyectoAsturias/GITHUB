41e76c9afa8d7eeb28a0feda7ebbc89e56fc8367
Alex <hueskyecla@hotmail.com>
2015-08-12 19:55:31 +0200

    update 12.8.15
    lo que faltaba

--- MapGenerator/js/map.js
+++ MapGenerator/js/map.js
@@ -413,51 +413,6 @@ function clearMap(){
 	});
 }
 
-function baseLayer(layerName){
-	removeOldBaseLayer();
-	if (layerName=="OSM"){
-    	var osmLayer = new ol.layer.Tile({
-    		source: new ol.source.OSM()
-    	});
-    	osmLayer.name = "OpenStreet Maps";
-    	osmLayer.base = true;
-    	map.getLayers().insertAt(0,osmLayer);
-    	var html="<span data-label-placement>OpenStreet Map</span> <span class=\"caret\"></span>";
-    	$("#baseLayerButton #baseButton").empty().append(html);
-   	}
-   	else if(layerName=="BAL"){
-   		var bingAerialLayer= new ol.layer.Tile({
-   			source: new ol.source.BingMaps({
-        		key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
-        		imagerySet:'Aerial'
-        	})
-   		})
-    	bingAerialLayer.name = "Bing Aerial Layer";
-    	bingAerialLayer.base = true;
-    	map.getLayers().insertAt(0,bingAerialLayer);
-    	var html="<span data-label-placement>Bing Aerial Layer</span> <span class=\"caret\"></span>";
-    	$("#baseLayerButton #baseButton").empty().append(html);
-   	}
-   	else if(layerName=="MQS"){
-   		var mapQuest= new ol.layer.Tile({
-      		source: new ol.source.MapQuest({layer: 'sat'})
-  		})
-  		mapQuest.name= "Map Quest Sat";
-  		mapQuest.base = true;
-  		map.getLayers().insertAt(0,mapQuest);
-    	var html="<span data-label-placement>Map Quest Satelite</span> <span class=\"caret\"></span>";
-    	$("#baseLayerButton #baseButton").empty().append(html);
-   	}
-}
-
-function removeOldBaseLayer(){
-	var layersArray=map.getLayers().getArray();
-	for(var i=0;i<layersArray.length;i++){
-		if(layersArray[i].base==true)
-			map.removeLayer(layersArray[i])
-	}
-}
-
 function getCookie(cname) {
     var name = cname + "=";
     console.log(document.cookie);
