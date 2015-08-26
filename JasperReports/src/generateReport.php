a210fb528f7ccc8a2cc2c4d0788404930439619c
José Braulio <josebragv@gmail.com>
2015-08-26 10:11:05 +0100

    Change preview with changes in modal window.

--- JasperReports/src/generateReport.php
+++ JasperReports/src/generateReport.php
@@ -22,6 +22,8 @@ if (isset($_GET["tag"])){
 }
 
 if (isset($_POST["reportName"])){
+    echo $_POST["reportDescription"];
+    return;
     startReport();
     addReportsField();
     exportToPdf();
@@ -50,8 +52,14 @@ function deleteReport(){
 }
 
 function startReport(){
-    global $reportGenerator;
-    $reportGenerator = new ReportGenerator(realpath(".")."/../reports/".$_POST["reportName"].".jrxml", realpath(".")."/../tmpReports/".$_POST["fileName"].".pdf", JAVA_INC_URL);
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    if (isset($_SESSION["reportGenerator"])){
+        global $reportGenerator;
+        $reportGenerator = $_SESSION["reportGenerator"];
+        $reportGenerator = new ReportGenerator(realpath(".")."/../reports/".$_POST["reportName"].".jrxml", $reportGenerator->outputPath, JAVA_INC_URL);
+    }
     $reportGenerator->initializeReport();
 }
 
@@ -84,9 +92,8 @@ function downloadPdf(){
 
 function addReportsField(){
     global $reportGenerator;
-    $reportGenerator->addReportTextField("title", "Título del informe");
-    $reportGenerator->addReportTextField("description", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit magna scelerisque ex consequat, ultrices vehicula augue placerat. Nam tincidunt, ligula id dapibus maximus, ipsum velit molestie ante, id viverra quam lectus quis magna. Quisque ac quam eu erat consectetur euismod ut ac nulla. Aenean vestibulum, arcu non viverra semper, tellus nibh facilisis sem, eu porta neque lacus nec urna. Sed sed pellentesque lorem. Quisque pulvinar aliquam lectus, a malesuada enim tincidunt ut. Aenean volutpat ac arcu et consequat. Aliquam tempus arcu non nibh pellentesque, cursus scelerisque ligula suscipit. Etiam neque massa, hendrerit at luctus eu, lacinia quis nisi. Ut pellentesque lorem in nisl sodales, quis ultrices dolor pretium. Vivamus vel mattis lacus, id lacinia enim.
-Generados 5 párrafos, 459 palabras, 3117 bytes de Lorem Ipsum");
+    $reportGenerator->addReportTextField("title", $_POST["reportTitle"]);
+    $reportGenerator->addReportTextField("description", $_POST["reportDescription"]);
 
     $imagedata = file_get_contents($_POST["mapImage"]);
     $reportGenerator->addReportImageField("mapImage", base64_encode($imagedata));
